cb3ab4da96d4a4f87e271e79afaf2b067c76213f
Alexandra <avgritsaenko_1@edu.hse.ru>
2026-02-07 15:01:24 +0300

    merge

---
+++ Backend/AuthServer/errors.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+)
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte(`{"error": "не нашел нихуя"}`))
+}
+
+func internalServerError(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte(`{"error": "сервер ебанулся, внутренняя"}`))
+}
+
+func badRequest(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadRequest)
+	w.Write([]byte(`{"error": "плохой запрос"}`))
+}
+
+func unauthorized(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusUnauthorized)
+	w.Write([]byte(`{"error": "нелегал"}`))
+}
---
+++ Backend/AuthServer/go.mod
@@ -0,0 +1,5 @@
+module github.com/alexandra-gritsaenko/gymbro-backend
+
+go 1.25.6
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
